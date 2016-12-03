f91583ec105897994bb3397f7f4b71085da45cff
Sandesh Pathak <sandeshpathak0112@gmail.com>
2016-12-03 11:08:48 -0500

    update

---
+++ karmaBarGraph.py
@@ -0,0 +1,34 @@
+import praw
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+reddit = praw.Reddit("A reddit karma visualizer by Sandesh")
+
+user_name = raw_input("Please enter a reddit username? ")
+
+redditor = reddit.get_redditor(user_name)
+content = redditor.get_submitted(limit=None)
+
+karma = {}
+
+for thing in content:
+    subreddit = thing.subreddit.display_name
+    karma[subreddit] = (karma.get(subreddit, 0) + thing.score)
+  
+
+objects = list(karma.keys())
+y_pos = np.arange(len(objects))
+print y_pos
+performance =  list(karma.values())
+ 
+plt.barh(y_pos, performance, align='center', alpha=0.5)
+plt.yticks(y_pos, objects)
+plt.xlabel('Karma')
+plt.title('Subreddit Bar Graph')
+ 
+plt.show()
+
+
+
